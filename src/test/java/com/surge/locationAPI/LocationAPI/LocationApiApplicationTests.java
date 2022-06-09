bd162133d2a5ce5e2c496fc637c622e911ed9610
Osagie Abel <osagieabel@icloud.com>
2022-06-09 16:35:03 +0100

    Initial Commit

---
+++ src/test/java/com/surge/locationAPI/LocationAPI/LocationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surge.locationAPI.LocationAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
